adba7284721c269e0dd292dc961eb7a057f3353a
Gabko14 <95317228+Gabko14@users.noreply.github.com>
2022-04-29 21:54:52 +0200

    Welcome card done

---
+++ generateimage.js
@@ -0,0 +1,48 @@
+const Canvas = require("canvas")
+const Discord = require("discord.js")
+const background = "https://i.pinimg.com/736x/c0/27/f2/c027f251134cfe057a5b9019e0bfc444.jpg"
+
+const dim = {
+    height: 500,
+    width: 800,
+    margin: 400
+}
+
+const av = {
+    size: 256,
+    x: 480,
+    y: 70
+}
+
+const generateImage = async (member) => {
+    let username = member.user.username
+    let discrim = member.user.discriminator
+    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size:av.size})
+
+    const canvas = Canvas.createCanvas(dim.width, dim.height)
+    const ctx = canvas.getContext("2d")
+
+    // draw in the background
+    const backimg = await Canvas.loadImage(background)
+    ctx.drawImage(backimg, 0, 0)
+
+    // draw black tinted box 
+    ctx.fillStyle = "rgba(0,0,0,0.0)"
+    ctx.fillRect(dim.margin, dim.margin, dim.width - 2 * dim.margin, dim.height - 2 * dim.margin)
+
+    const avimg = await Canvas.loadImage(avatarURL)
+    ctx.save()
+
+    ctx.beginPath()
+    ctx.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.clip()
+
+    ctx.drawImage(avimg, av.x, av.y)
+    ctx.restore()
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    return attachment
+}
+
+module.exports = generateImage
